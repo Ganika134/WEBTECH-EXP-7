e6e5aac8fd7b3a75ee880746d1f2abc32c284bd0
Ganika134 <ganikap@karunya.edu.in>
2025-10-22 09:17:22 +0530

    initial commit

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/WEBTECH-EXP-7-calculator2/',
+    plugins: [react()],
 })
